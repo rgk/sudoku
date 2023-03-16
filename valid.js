f4a09dfb9b08fb1a3557b2c1127223f4e69f2fdb
Robert Kowalski <rgkmg25@gmail.com>
2023-03-15 23:34:25 -0400

    Update valid.js

--- valid.js
+++ valid.js
@@ -1,10 +1,25 @@
-function isValid(board) {
+export function isValid(board) {
   const size = { x: board.length, y: board[0].length };
   const total = 45;
   
-  for (let i = 0; i < size.x; i++) {
-    if (board[i].reduce((sum, num) => sum + num) !== total) return false;
+  for (let row = 0, totalX = 0, totalY = 0; row < size.x; row++) {
+    for (let col = 0; col < size.y; col++) {
+      totalX += board[row][col];
+      totalY += board[col][row];
+    }
+    if (totalX !== total || totalY !== total) return false;
   }
 
   return true;
+}
+
+function findEmpty(board) {
+  const size = { x: board.length, y: board[0].length };
+  for (let row = 0; row < size.x; row++) {
+    for (let col = 0; col < size.y; col++) {
+      if (board[row][col] === 0) return [row, col];
+    }
+  }
+
+  return null;
 }
