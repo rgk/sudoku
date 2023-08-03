53d1d9eb8f1b1559f0cbf3851c77e303995abb1b
Robert Kowalski <rgkmg25@gmail.com>
2023-08-02 23:32:17 -0400

    Basic setup for the board.

---
+++ index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+import { update } from './index.js';
+
+</script>
+
+<template>
+    <div id="board" @click="update">
+        <div class="section">
+            <div class="cell">
+
+            </div>
+        </div>
+    </div>
+</template>
