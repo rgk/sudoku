10e3b2ef0166fffaa8fd564e5f17ab890c4de680
Robert Kowalski <rgkmg25@gmail.com>
2023-08-06 23:53:58 -0400

    Some setup.

--- index.vue
+++ index.vue
@@ -1,19 +1,29 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 
-import { update } from './index.js';
+import { update, restart } from './index.js';
+
+onMounted(() => {
+  console.log('soduku loaded');
+});
 
 </script>
 
 <template>
-    <div id="board" @click="update">
-        <div v-for="i in 9" class="section">
-            <div v-for="j in 9" class="cell">
-                <input>
-            </div>
-        </div>
+  <div id="board">
+    <div v-for="i in 9" class="section">
+      <div v-for="j in 9" class="cell">
+        <input @click="update">
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
+  #board {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    grid-gap: 2px;
+  }
 </style>
